1ac3d4159b76e97cbb45063579429aba26f42696
gogoing-code <1378412050@qq.com>
2022-01-31 17:35:31 +0800

    feat - context

---
+++ Practice/day2-context/gee/gee.go
@@ -0,0 +1,43 @@
+package gee
+
+import "net/http"
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(*Context)
+
+// Engine 实现 ServeHTTP 接口
+type Engine struct {
+	router *router
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+// addRoute 添加路由信息
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+// GET 解析 GET 请求
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST 解析 POST 请求
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run 定义一个开启 http server 的方法
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// 实现 ServeHTTP 接口
+// PS：实现了接口方法的 struct 都可以强制转换为接口类型
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
---
+++ Practice/day2-context/gee/router.go
@@ -0,0 +1,31 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+// 提取路由相关方法和结构，方便后续对路由功能进行增强
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc)}
+}
+
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	key := method + "-" + pattern
+	r.handlers[key] = handler
+}
+
+func (r *router) handle(c *Context) {
+	key := c.Method + "-" + c.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
+	}
+}
